0fda908530ff28e8e27efa1bdd47bf742276c71a
Charles Chen <117331377+cookacola@users.noreply.github.com>
2023-07-13 19:17:48 -0500

    Update contact

--- frontend/src/app/gallery/[slug]/page.jsx
+++ frontend/src/app/gallery/[slug]/page.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 import { micromark } from "micromark";
 
 async function getPiece(pieceSlug) {
@@ -14,20 +15,23 @@ export default async function Piece({ params }) {
 	const descriptionHTML = micromark(piece.data[0].attributes.description);
 
 	return (
-		<div className="mt-16 flex flex-col items-center gap-8 w-2/3 mx-auto">
+		<div className="flex flex-col items-center w-2/3 gap-8 mx-auto mt-16">
 			<div className="">
 				<img
 					src={`http://127.0.0.1:1337${piece.data[0].attributes.artwork.data[0].attributes.url}`}
 					alt={`http://127.0.0.1:1337${piece.data[0].attributes.artwork.data[0].attributes.name}`}
 				/>
 			</div>
-			<div className="flex flex-col gap-5 items-center">
+			<div className="flex flex-col items-center gap-5">
 				<h1>{piece.data[0].attributes.title}</h1>
 				<h1>{piece.data[0].attributes.medium}</h1>
 				<h2>{piece.data[0].attributes.year}</h2>
-				<div className="text-white mb-10">
+				<div className="mb-10 text-white">
 					<div dangerouslySetInnerHTML={{ __html: descriptionHTML }}></div>
 				</div>
+				<Link href="/gallery" className="mb-10">
+					Back
+				</Link>
 			</div>
 		</div>
 	);
