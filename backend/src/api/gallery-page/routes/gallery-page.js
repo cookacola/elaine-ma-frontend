664aea35b03279708f6d5b5fd9f63c6f4fa798c2
Charles Chen <117331377+cookacola@users.noreply.github.com>
2023-07-14 16:32:41 -0500

    Removed backend

--- backend/src/api/gallery-page/routes/gallery-page.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * gallery-page router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::gallery-page.gallery-page');
